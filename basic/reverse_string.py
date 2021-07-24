8ef409dace5117e3a38badc8a372f02b959822dc
seosuwan <los2ve7@gmail.com>
2021-07-24 16:34:45 +0900

    july24

--- basic/reverse_string.py
+++ basic/reverse_string.py
@@ -2,9 +2,14 @@ def str_to_list(payload: str) -> []:
     return [i for i in payload if i.isalnum()]
 
 
-def reverse_string(s:[]) -> []:
-        s = s[::-1]
-        return s
+def reverse_string(s: []) -> None:
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left],s[right] = s[right],s[left]
+        left += 1
+        right -= 1
+    return s
+
 def list_to_str(s: []) -> str:
     pass
 
@@ -13,5 +18,9 @@ def list_to_str(s: []) -> str:
 if __name__ == '__main__':
     re = str_to_list('HELLO WORLD')
     print(re)
+    print(re)
+    print(re)
+    print(re)
     re1 = reverse_string(re)
     print(re1)
+
