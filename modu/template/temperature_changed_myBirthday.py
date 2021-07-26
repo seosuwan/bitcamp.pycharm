8b48b9deb5a7602d87493454a6f4a31489f4a395
seosuwan <los2ve7@gmail.com>
2021-07-26 18:55:56 +0900

    list_git

--- modu/template/temperature_changed_myBirthday.py
+++ modu/template/temperature_changed_myBirthday.py
@@ -13,8 +13,8 @@ def save_highest_temperature(self):
 data : [] = list
 '''
 class TemperatureChangedMyBirthday(object):
-    data: [] = list()
-    highest_temperature: [] = list()
+    data: [] = list() #csv를 담을 data배열
+    highest_temperature: [] = list() # 최고기온을 담을 배열
 
     def processing(self):  #훅을 걸어서 메소드 순서정하기
         self.read_data
@@ -23,18 +23,18 @@ class TemperatureChangedMyBirthday(object):
         self.extracting_date_data
 
     def read_data(self) -> object:
-        data = csv.reader(open('./data/unit_5_seoul.csv', 'rt', encoding = 'UTF-8'))
+        data = csv.reader(open('./data/unit_5_seoul.csv', 'rt', encoding='UTF-8'))
         next(data)
        # print([i for i in data])
         self.data = data
         return data
 
     def show_highest_temperature(self):
-        return [i[-1] for i in self.data]
+        return [i[-1] for i in self.data] # i에 담겨있는 -1번째 인덱스를 모두 가져와
 
     def save_highest_temperature(self):
-        [self.highest_temperature.append(float(i[-1])) for i in self.data if i[-1] != '']
-        print({len(self.highest_temperature)})
+        [self.highest_temperature.append(float(i[-1])) for i in self.data if i[-1] != '']#1. 배열에 담는것 2.조건이있다는것
+        print({len(self.highest_temperature)}) #최고기온을 담은 배열에 실수로된 -1인덱스 와 모든 데이터를 담은 data에서 마지막 인덱스와 비교해서 공백이 없으면 갖구왕
 
     def visualizing_data_highest_temperature(self):
         plt.plot(self.highest_temperature(), 'r')
@@ -42,14 +42,14 @@ class TemperatureChangedMyBirthday(object):
         plt.show()
 
     def highest_temperatures_my_birthday(self):
-        high = []
-        low = []
+        high = [] #  최고기온을 담을 배열
+        low = [] #최저기온을 담을 배열
         for i in self.data:
-            if i[-1] != '' and i[-2] != '':
-                if 1983 <= int(i[0].split('-')[0]):
-                    if (i[0].split('-')[1] == '02') and i[0].split('-')[2] == '14':
-                        high.append(float(i[-1]))
-                        low.append(float(i[-2]))
+            if i[-1] != '' and i[-2] != '': # 조건문 csv에 있는 인덱스 -1 뒤에서 첫번째 와 두번째가 공백이 없을경우
+                if 1983 <= int(i[0].split('-')[0]): # 정수로된 csv에 0번째를 분할해서 분할한것에 인덱스 0번째를 1983보다 작거나 같을때 가지고 와 split()은 기본적으로 문자열공백 자름
+                    if (i[0].split('-')[1] == '02') and i[0].split('-')[2] == '14':# csv에 0번째 인덱스 를 자른것에 1번째 인덱스와  02를 비교 그리고 2번째인덱스와 14를 비교
+                        high.append(float(i[-1])) #실수로 된 -1번째 인덱스를 high에 담아라
+                        low.append(float(i[-2])) #실수로 된 -2번째 인덱스를 low에 담아라
         plt.rc('font', family='Malgun Gothic')
         plt.rcParams['axes.unicode_minus'] = False
         plt.title('내 생일 기온 변화 그래프')
@@ -66,8 +66,9 @@ def date(full_date):
 if __name__ == '__main__':
     this = TemperatureChangedMyBirthday()
     this.read_data()
+    print(this.show_highest_temperature())
     #this.save_highest_temperature()
-    this.highest_temperatures_my_birthday()
+    #this.highest_temperatures_my_birthday()
 
 
 
