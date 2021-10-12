be416b844a45dea6c1493118c0bbbdbaec5f1124
seosuwan <los2ve7@gmail.com>
2021-10-12 18:49:35 +0900

    18:50

---
+++ lecture/grade/views.py
@@ -0,0 +1,13 @@
+from lecture.grade.models import Person
+
+if __name__ == '__main__':
+    persons = []
+    while 1:
+        menu = input('0-EXIT 1- Add 2- Print')
+        if menu == '0':
+            break
+        elif menu == '1':
+            persons.append(Person(input('name'), input('age'), input('address')))
+        elif menu == '2':
+            for i in persons:
+                i.to_string()
